8cf8c49e6fcbf60dac2d20dea9d7c1f86fc88ca9
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-04-22 15:40:45 +0900

    完成品

--- main.cpp
+++ main.cpp
@@ -1,53 +1,140 @@
 #include <Novice.h>
+#include <assert.h>
 #include <math.h>
 #include <stdio.h>
 const char kWindowTitle[] = "LC1C_18_フジイ_コハク";
+// Matrix4x4
+struct Matrix4x4 {
+  float m[4][4];
+};
 
+struct Vector3 {
+  float x, y, z;
+};
+
+Vector3 translate{4.1f, 2.6f, 0.8f};
+Vector3 scale{1.5f, 5.2f, 7.3f};
+// 平行移動行列
+Matrix4x4 MakeTranslateMatrix(const Vector3 &tlanslate) {
+  Matrix4x4 result = {};
+  result.m[0][0] = 1.0f;
+  result.m[1][1] = 1.0f;
+  result.m[2][2] = 1.0f;
+  result.m[3][3] = 1.0f;
+  result.m[3][0] = tlanslate.x;
+  result.m[3][1] = tlanslate.y;
+  result.m[3][2] = tlanslate.z;
+
+  return result;
+}
+// 拡大縮小行列
+Matrix4x4 rix4x4MakeScaleMatrix(const Vector3 &s) {
+  Matrix4x4 result = {};
+  result.m[0][0] = s.x;
+  result.m[1][1] = s.y;
+  result.m[2][2] = s.z;
+  result.m[3][3] = 1.0f;
+  return result;
+}
+// 座標変換
+Vector3 Transform(const Vector3 &vector, const Matrix4x4 &matrix) {
+  Vector3 result;
+  result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] +
+             vector.z * matrix.m[2][0] + 1.0f * matrix.m[3][0];
+
+  result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] +
+             vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];
+
+  result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] +
+             vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];
+
+  float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] +
+            vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];
+  assert(w != 1.0f);
+  result.x /= w;
+  result.y /= w;
+  result.z /= w;
+
+  return result;
+}
+// 表示関数
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+void MatrixScreenPrintf(int x, int y, const Matrix4x4 &matirix,
+                        const char *label) {
+  Novice::ScreenPrintf(x, y, "%s", label);
+  for (int row = 0; row < 4; ++row) {
+    for (int column = 0; column < 4; ++column) {
+      Novice::ScreenPrintf(x + column * kColumnWidth,
+                           y + (row + 1) * kRowHeight, "%6.02f",
+                           matirix.m[row][column]);
+    }
+  }
+}
+// 表示関数
+// VectorScreenPrintf関数
+static const int kColumWidth = 60;
+void VectorScreenPrintf(int x, int y, const Vector3 &vector,
+                        const char *label) {
+  Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+  Novice::ScreenPrintf(x + kColumWidth, y, "%.02f", vector.y);
+  Novice::ScreenPrintf(x + kColumWidth * 2, y, "%.02f", vector.z);
+  Novice::ScreenPrintf(x + kColumWidth * 3, y, "%s", label);
+}
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+  // ライブラリの初期化
+  Novice::Initialize(kWindowTitle, 1280, 720);
 
-	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+  // キー入力結果を受け取る箱
+  char keys[256] = {0};
+  char preKeys[256] = {0};
+  Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+  Matrix4x4 scaleMatrix = rix4x4MakeScaleMatrix(scale);
+  Vector3 point{2.3f, 3.8f, 1.4f};
+  Matrix4x4 transFormMatrix = {1.0f, 2.0f, 3.0f, 4.0f, 3.0f, 1.0f, 1.0f, 2.0f,
+                               1.0f, 4.0f, 2.0f, 3.0f, 2.0f, 2.0f, 1.0f, 3.0f};
 
-	// ウィンドウの×ボタンが押されるまでループ
-	while (Novice::ProcessMessage() == 0) {
-		// フレームの開始
-		Novice::BeginFrame();
+  Vector3 transFormed = Transform(point, transFormMatrix);
 
-		// キー入力を受け取る
-		memcpy(preKeys, keys, 256);
-		Novice::GetHitKeyStateAll(keys);
+  // ウィンドウの×ボタンが押されるまでループ
+  while (Novice::ProcessMessage() == 0) {
+    // フレームの開始
+    Novice::BeginFrame();
 
-		///
-		/// ↓更新処理ここから
-		///
+    // キー入力を受け取る
+    memcpy(preKeys, keys, 256);
+    Novice::GetHitKeyStateAll(keys);
 
-		///
-		/// ↑更新処理ここまで
-		///
+    ///
+    /// ↓更新処理ここから
+    ///
 
-		///
-		/// ↓描画処理ここから
-		///
+    ///
+    /// ↑更新処理ここまで
+    ///
 
-		///
-		/// ↑描画処理ここまで
-		///
+    ///
+    /// ↓描画処理ここから
+    ///
+    VectorScreenPrintf(0,500, transFormed, "transformed");
+    MatrixScreenPrintf(0, 2, translateMatrix, "translateMatrix");
+    MatrixScreenPrintf(0, kRowHeight * 6, scaleMatrix, "scaleMatrix");
+    ///
+    /// ↑描画処理ここまで
+    ///
 
-		// フレームの終了
-		Novice::EndFrame();
+    // フレームの終了
+    Novice::EndFrame();
 
-		// ESCキーが押されたらループを抜ける
-		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
-			break;
-		}
-	}
+    // ESCキーが押されたらループを抜ける
+    if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+      break;
+    }
+  }
 
-	// ライブラリの終了
-	Novice::Finalize();
-	return 0;
+  // ライブラリの終了
+  Novice::Finalize();
+  return 0;
 }
