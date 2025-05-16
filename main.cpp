1f6388ede74efee1cd825d4aa5c22c84791e5ef6
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-05-16 16:40:16 +0900

    何も変更なし

--- main.cpp
+++ main.cpp
@@ -515,6 +515,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     // 透視投影行列の計算
     Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(
         float(0.45), float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+
     // GRID計算
     // Grid用のWorldViewProjection行列（ワールド行列は単位行列）
     Matrix4x4 gridWorldMatrix = MakeIdentity4x4(); // 単位行列（何も変換しない）
