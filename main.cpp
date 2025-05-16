f4ce6a8eef7363af8e8b072197525dd482880fe5
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-05-16 16:37:11 +0900

    セーブ

--- main.cpp
+++ main.cpp
@@ -423,7 +423,7 @@ Vector3 cameraTransLate{0.0f, 1.9f, -6.49f};
 Vector3 cameraRotate{0.26f, 0.0f, 0.0f};
 
 Sphere sphere = {
-    {0.0f, 2.0f, 0.0f}, // center（x, y, z）
+    {0.0f, 0.0f, 0.0f}, // center（x, y, z）
     1.0f                // radius
 };
 
