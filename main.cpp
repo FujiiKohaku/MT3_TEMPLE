a5bbba7b51a6e628eb4972ec97a844b7d31c6c6c
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-05-30 15:16:24 +0900

    正解

--- main.cpp
+++ main.cpp
@@ -10,43 +10,39 @@ struct Vector3 {
   float x, y, z;
 };
 
-// X軸回転行列
+// X軸回転行列（左手座標系）
 Matrix4x4 MakeRotateXMatrix(float radian) {
   Matrix4x4 result = {};
-
   result.m[0][0] = 1.0f;
-  result.m[1][1] = std::cos(radian);
-  result.m[1][2] = std::sin(radian);
-  result.m[2][1] = -std::sin(radian);
-  result.m[2][2] = std::cos(radian);
+  result.m[1][1] = cosf(radian);
+  result.m[1][2] = sinf(radian);  // ← ここ符号変更
+  result.m[2][1] = -sinf(radian); // ← ここ符号変更
+  result.m[2][2] = cosf(radian);
   result.m[3][3] = 1.0f;
-
   return result;
 }
-// Y軸回転行列
+
+// Y軸回転行列（左手座標系）
 Matrix4x4 MakeRotateYMatrix(float radian) {
   Matrix4x4 result = {};
-
-  result.m[0][0] = std::cos(radian);
-  result.m[0][2] = std::sin(radian);
+  result.m[0][0] = cosf(radian);
+  result.m[0][2] = -sinf(radian); // ← ここ符号変更
   result.m[1][1] = 1.0f;
-  result.m[2][0] = -std::sin(radian);
-  result.m[2][2] = std::cos(radian);
+  result.m[2][0] = sinf(radian); // ← ここ符号変更
+  result.m[2][2] = cosf(radian);
   result.m[3][3] = 1.0f;
-
   return result;
 }
-// Z軸回転行列
+
+// Z軸回転行列（左右手共通）
 Matrix4x4 MakeRotateZMatrix(float radian) {
   Matrix4x4 result = {};
-
-  result.m[0][0] = std::cos(radian);
-  result.m[0][1] = -std::sin(radian);
-  result.m[1][0] = std::sin(radian);
-  result.m[1][1] = std::cos(radian);
+  result.m[0][0] = cosf(radian);
+  result.m[0][1] = sinf(radian);  // 符号が逆
+  result.m[1][0] = -sinf(radian); // 符号が逆
+  result.m[1][1] = cosf(radian);
   result.m[2][2] = 1.0f;
   result.m[3][3] = 1.0f;
-
   return result;
 }
 // 行列の積
