915563c8d86272d93323ff7ead1c5b663e7f6383
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-07-13 21:17:30 +0900

    壁にあったら速度の向きを反転する資格を作成

--- main.cpp
+++ main.cpp
@@ -6,48 +6,75 @@ const char kWindowTitle[] = "LC1C_18_フジイ_コハク";
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+  // ライブラリの初期化
+  Novice::Initialize(kWindowTitle, 1280, 720);
 
-	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+  // キー入力結果を受け取る箱
+  char keys[256] = {0};
+  char preKeys[256] = {0};
 
-	// ウィンドウの×ボタンが押されるまでループ
-	while (Novice::ProcessMessage() == 0) {
-		// フレームの開始
-		Novice::BeginFrame();
+  //==定義==//
+  // プレイヤーの位置
+  int posX;
+  int posY;
+  // プレイヤー縦横幅
+  int width = 64;
+  int height = 64;
+  // プレイヤーの速度
+  int speed;
+  //==初期化==//
+  posX = 640; // ウィンドウの中心に配置
+  posY = 360; // ウィンドウの中心に配置
+  speed = 5;  // プレイヤーの移動速度
+  //==初期化ここまで==//
 
-		// キー入力を受け取る
-		memcpy(preKeys, keys, 256);
-		Novice::GetHitKeyStateAll(keys);
+  // ウィンドウの×ボタンが押されるまでループ
+  while (Novice::ProcessMessage() == 0) {
+    // フレームの開始
+    Novice::BeginFrame();
 
-		///
-		/// ↓更新処理ここから
-		///
+    // キー入力を受け取る
+    memcpy(preKeys, keys, 256);
+    Novice::GetHitKeyStateAll(keys);
 
-		///
-		/// ↑更新処理ここまで
-		///
+    ///
+    /// ↓更新処理ここから
+    ///
 
-		///
-		/// ↓描画処理ここから
-		///
+    // プレイヤーの移動処理
+    posX += speed;
+    // 右壁に衝突したときの処理
+    if (posX >= 1280 - width) {
+      speed *= -1; // 速度を反転
+    }
+    // 左壁に衝突したときの処理
+    if (posX <= 0) {
 
-		///
-		/// ↑描画処理ここまで
-		///
+      speed *= -1; // 速度を反転
+    }
 
-		// フレームの終了
-		Novice::EndFrame();
+    ///
+    /// ↑更新処理ここまで
+    ///
 
-		// ESCキーが押されたらループを抜ける
-		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
-			break;
-		}
-	}
+    ///
+    /// ↓描画処理ここから
+    ///
+    Novice::DrawBox(posX, posY, width, height, 0.0f, WHITE, kFillModeSolid);
+    ///
+    /// ↑描画処理ここまで
+    ///
 
-	// ライブラリの終了
-	Novice::Finalize();
-	return 0;
+    // フレームの終了
+    Novice::EndFrame();
+
+    // ESCキーが押されたらループを抜ける
+    if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+      break;
+    }
+  }
+
+  // ライブラリの終了
+  Novice::Finalize();
+  return 0;
 }
