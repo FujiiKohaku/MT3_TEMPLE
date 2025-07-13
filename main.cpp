d6ba2c3d154220f1d52ee3b82f0bff415301d8bb
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-07-13 23:08:54 +0900

    球発射装置完成

--- main.cpp
+++ main.cpp
@@ -18,21 +18,31 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   float posX;
   float posY;
   // プレイヤー縦横幅
-
   int radius = 32; // 楕円の半径
   // プレイヤーの速度
   float speed;
   float length;
+  // 方向ベクトル
+  float moveX;    // プレイヤーの移動方向X
+  float moveY;    // プレイヤーの移動方向Y
+  float newMoveX; // プレイヤーの新しい移動方向X
+  float newMoveY; // プレイヤーの新しい移動方向Y
   //==初期化==//
-  posX = 640; // ウィンドウの中心に配置
-  posY = 360; // ウィンドウの中心に配置
-  speed = 5;  // プレイヤーの移動速度
-  length = 0; // プレイヤーの移動距離
-  //==ベクトル==//
-  float moveX = 0;    // プレイヤーの移動方向X
-  float moveY = 0;    // プレイヤーの移動方向Y
-  float newMoveX = 0; // プレイヤーの新しい移動方向X
-  float newMoveY = 0; // プレイヤーの新しい移動方向Y
+  posX = 640;   // ウィンドウの中心に配置
+  posY = 360;   // ウィンドウの中心に配置
+  speed = 5;    // プレイヤーの移動速度
+  length = 0;   // プレイヤーの移動距離
+  moveX = 0;    // プレイヤーの移動方向X
+  moveY = 0;    // プレイヤーの移動方向Y
+  newMoveX = 0; // プレイヤーの新しい移動方向X
+  newMoveY = 0; // プレイヤーの新しい移動方向Y
+
+  // 弾
+  float bulletPosX = 0;
+  float BulletPosY = 0;
+  float bulletSpeed = 8;
+  int bulletRadius = 16;
+  int isBulletShot = false;
 
   //==初期化ここまで==//
 
@@ -65,14 +75,31 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     if (keys[DIK_D]) {
       moveX = 1.0f; // 右に移動
     }
+    //==スぺスキー押したら弾を発射==//
+    if (!isBulletShot) {
 
+      if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
+        bulletPosX = posX; // プレイヤーの位置を弾の初期位置に設定
+        BulletPosY = posY; // プレイヤーの位置を弾の初期位置に設定
+
+        isBulletShot = true;
+      }
+      //フラグがONになったら弾の移動処理
+    }
+    if (isBulletShot) {
+      BulletPosY -= bulletSpeed;
+    }
+
+    if (BulletPosY <= 0-bulletRadius) {
+      isBulletShot = false; // 弾が画面外に出たら発射状態を解除
+    }
     // 移動ベクトルの長さ（距離）
     length = sqrtf(moveX * moveX + moveY * moveY);
 
     if (length != 0.0f) {
       newMoveX = moveX / length; // 正規化
       newMoveY = moveY / length; // 正規化
-    } 
+    }
 
     posX += newMoveX * speed; // 新しい位置に移動
     posY += newMoveY * speed; // 新しい位置に移動
@@ -86,6 +113,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     ///
     Novice::DrawEllipse(static_cast<int>(posX), static_cast<int>(posY), radius,
                         radius, 0.0f, WHITE, kFillModeSolid);
+
+    Novice::DrawEllipse(static_cast<int>(bulletPosX),
+                        static_cast<int>(BulletPosY), bulletRadius,
+                        bulletRadius, 0.0f, RED, kFillModeSolid);
     ///
     /// ↑描画処理ここまで
     ///
